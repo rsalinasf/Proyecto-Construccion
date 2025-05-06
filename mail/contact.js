8cbf66a20d8bab39683436eed6a99eac41bfd163
Nxssty <ricardopigmedia@gmail.com>
2025-05-06 17:21:26 -0400

    arreglos de mail

--- mail/contact.js
+++ mail/contact.js
@@ -1,17 +1,20 @@
 $(function () {
-
     $("#contactFormPc input, #contactFormPc textarea, #contactFormMobile input, #contactFormMobile textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
+            // manejar errores
         },
         submitSuccess: function ($form, event) {
             event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
 
-            $this = $("#sendMessageButton");
+            const isMobile = $form.attr('id') === 'contactFormMobile';
+
+            const name = isMobile ? $("#nameMobile").val() : $("#name").val();
+            const email = isMobile ? $("#emailMobile").val() : $("#email").val();
+            const subject = isMobile ? $("#subjectMobile").val() : $("#subject").val();
+            const message = isMobile ? $("#messageMobile").val() : $("#message").val();
+            const $this = isMobile ? $("#sendMessageButtonMobile") : $("#sendMessageButton");
+
             $this.prop("disabled", true);
 
             $.ajax({
@@ -26,21 +29,19 @@ $(function () {
                 cache: false,
                 success: function () {
                     $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Su mensaje ha sido enviado con exito! </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
+                        .html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>")
+                        .append("<strong>¡Su mensaje ha sido enviado con éxito!</strong>")
+                        .append('</div>');
+                    $form.trigger("reset");
                 },
                 error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Lo sentimos " + name + ", parece que nuestro servidor de correo no responde. ¡Inténtalo más tarde!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
+                    $('#success > .alert-danger')
+                        .html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>")
+                        .append($("<strong>").text("Lo sentimos " + name + ", parece que nuestro servidor de correo no responde. ¡Inténtalo más tarde!"))
+                        .append('</div>');
+                    $form.trigger("reset");
                 },
                 complete: function () {
                     setTimeout(function () {
@@ -51,7 +52,7 @@ $(function () {
         },
         filter: function () {
             return $(this).is(":visible");
-        },
+        }
     });
 
     $("a[data-toggle=\"tab\"]").click(function (e) {
@@ -60,6 +61,7 @@ $(function () {
     });
 });
 
-$('#name').focus(function () {
+// Limpia mensajes de éxito/error cuando el usuario comienza a escribir su nombre
+$('#name, #nameMobile').focus(function () {
     $('#success').html('');
 });
